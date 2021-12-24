a9b39a5fe1d642ff075b05f54dc7d80ee879631e
alfon08 <alfonghs@gmail.com>
2021-12-24 12:08:16 +0100

    Changed name

--- elec35x-c1-2021-main-Ryans coding-Alfon/azure_cloud_credentials.h
+++ elec35x-c1-2021-main-Ryans coding-Alfon/azure_cloud_credentials.h
@@ -14,7 +14,7 @@ namespace azure_cloud {
 
         // Use https://dpsgen.z8.web.core.windows.net/ to obtain YOUR connection string
         // This one will not work, but I guess you knew that ;)
-        const char iothub_connection_string[] = "write connection string";
+        const char iothub_connection_string[] = "HostName=iotc-3e7caa92-161e-4bba-8ae9-b5ff970534fa.azure-devices.net;DeviceId=16ae64elodr;SharedAccessKey=pdobpLlJagqmmTGXGxL+qauh7YmFhXNyvClE/6guVsk=";
     }
 }
 #endif
