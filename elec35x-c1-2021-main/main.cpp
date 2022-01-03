c9c414e9056edba97d48589c333d6fa2a3cc060d
ryan569 <75443275+ryan569@users.noreply.github.com>
2022-01-03 09:55:14 +0000

    NEW UPDATE

--- elec35x-c1-2021-main/main.cpp
+++ elec35x-c1-2021-main/main.cpp
@@ -19,31 +19,13 @@
 
 InterruptIn Bluebtn(USER_BUTTON);           //used for printing out what is in the SD card
 InterruptIn btnA(BTN1_PIN);                 //used for printing out what is in the SD card
-InterruptIn CIEbutton (BTN2_PIN);           //Critical error button interrupt
 Sampling ldr(AN_LDR_PIN);                   // constructor for setting up Sampling
 buffer mes(0, 0, 0.0, 0.0);                 // constructor for setting up the buffer
-DigitalInOut redLed2 (TRAF_RED2_PIN);
-
+LCD_16X2_DISPLAY disp;
 Mail<buffer, 16> mail_box;                  //buffer holds 16 of type buffer
-
 int SDwriteFreq = 16;                        //writes to SD card afer x number of samples
-int numberSamples = 0;
-volatile int CErrorcount ;                  //critical error count
-const uint32_t RESET_TIME = 30000;          //30 sec countdown for watchdog
-
-//matrix
-extern char Lightarray[8];
-extern char Temparray[8];
-extern char Pressarray[8];
-char y = 'L'; //matrix array set to light by default
-
 microseconds sampRate = 100ms;              //sampling Rate
-microseconds tmrUpdate = 0ms;               //time to compare 
-Buzzer critAlarm;                           //to set of when critical error occurs
-EventQueue mainQueue;
-
-extern void BuzzStop();                     //call function to stop buzzer
-
+int numberSamples = 0;
 //Timers
 Ticker Samptick;                            //ISR for triggering sampling
 Timer updatetmr;                            //Timer for triggering update 
@@ -53,18 +35,31 @@ Thread t1(osPriorityAboveNormal);            //Sampling Thread
 Thread t2(osPriorityNormal);                 //Buffer Thread
 Thread t3(osPriorityNormal);                 //SD card Thread
 Thread t4(osPriorityNormal);                 //IOTHub Thread
-Thread t5(osPriorityNormal);                 //critical error thread
-Thread t6(osPriorityNormal);                 //matrix thread
 
 //* all in one place for ease. look for *
+Thread t5(osPriorityNormal);                //critical error thread
+Thread t6(osPriorityNormal);
+DigitalInOut redLed2 (TRAF_RED2_PIN);
+volatile int CErrorcount ;                  //critical error count
+const uint32_t RESET_TIME = 30000;          //30 sec countdown for watchdog
+InterruptIn CIEbutton (BTN2_PIN);
+Buzzer alarmm;
+extern int Lightarray[8];
+extern float Temparray[8];
+extern float Pressarray[8];
+extern int iotLight;
+char y = 'L'; //matrix array set to light by default
+microseconds tmrUpdate = 0ms;               //time to compare 
+EventQueue mainQueue;
+extern void BuzzStop();
 
-void Flag_Set(){                            //Sets the flag to start sampling via ticker
+
+
+void Flag_Set(){                            //Sets the flag to start sampling
      t1.flags_set(1);}
 
-void Flag_Set2(){                            //Sets the flag to start sampling via ticker
+void Flag_Set2(){                            //Sets the flag to start sampling
      t3.flags_set(1);}
-
- 
 
 //Thread 1
 void GetSample(){
@@ -93,6 +88,7 @@ void bufferSample(){
                 //Record average samples in buffer
                 mes.SpaceAllocate(ldr.Samptime_date, ldr.dataAVG.ldrEngAVG, ldr.dataAVG. TempAVG, ldr.dataAVG.PressAVG);//Allocate date, ldr, temp and pres in the buffer
                 mes.checkvalues(ldr.dataAVG.ldrEngAVG, ldr.dataAVG. TempAVG, ldr.dataAVG.PressAVG); //Check if they are above or below limits->WARNING
+                mes.updatearrays(ldr.dataAVG.ldrEngAVG, ldr.dataAVG. TempAVG, ldr.dataAVG.PressAVG);
                 t4.flags_set(1);            // set flag for updating data on IOTHub
                 
                 i = ++i;                    // increement counter
@@ -121,21 +117,22 @@ void iotazure(){
 void matrix_display() {
     matrix_bar start;
     while(true){
-    if(y == 'L'){
-        for(int i= 0; i<7; i++){
-        start.BarLight(Lightarray[i], i);
-        ThisThread::sleep_for(5000ms);
-        }
-    }
-    if(y == 'T'){
-        for(int i= 0; i<7; i++){
-        start.BarLight(Temparray[i], i);
-        ThisThread::sleep_for(5000ms);}}
-    }
-    if(y == 'P'){
-        for(int i= 0; i<7; i++){
-        start.BarLight(Pressarray[i], i);
-        ThisThread::sleep_for(5000ms);}}
+    start.clearMatrix();
+    disp.cls();
+    disp.locate(1, 0);
+    disp.printf("Display=%c\n",y);
+        if(y == 'L'){
+            for(int i= 0; i<7; i++){
+            start.BarLight(Lightarray[i], i);
+        }}
+        if(y == 'T'){
+            for(int i= 0; i<7; i++){
+            start.BarTemp(Temparray[i], i);
+            }}
+        if(y == 'P'){
+            for(int i= 0; i<7; i++){
+            start.BarPres(Pressarray[i], i);
+        }}}
 }
 
 // * thread 5 *
@@ -143,62 +140,54 @@ void CritError(){
     ThisThread::flags_wait_any(2); //block until this flag is set
     ThisThread::sleep_for(50ms);    //switch bounce to stabilise
     ThisThread::flags_clear(2);    //clear flag incase there were any due to switch bounce
-    CriticalSectionLock::enable();
     CErrorcount++;
-    CriticalSectionLock::disable();
-
 
     if (CErrorcount >=4) {
         CriticalSectionLock::enable();  //lock so no interrupts can interrupt it as ticker is an interrupt
-        //maybe a mutex lock    -   incase the other threads get calledx    
         printf ("critical error has occured. system will restart in 30 seconds");
-        alarmm.playTone("A", Buzzer::HIGHER_OCTAVE);      //if this doesn't work, probs just need to be initilised here to extern it as its in other file
+        alarmm.playTone("C", Buzzer::LOWER_OCTAVE);      //if this doesn't work, probs just need to be initilised here to extern it as its in other file
         redLed2=0;
         //CErrorcount=0;
-        Watchdog &watchdog = Watchdog::get_instance();  //gets instance of time referenced for the wtachdog
-        watchdog.start(RESET_TIME);                     //starts watchdog timer for 30 sec. whole system resets after time is over
-        //or
-        //wait_us(30000);
-        //system_reset();
-        //CIEbutton.waitforpress();                     //block by waiting for press
+        Watchdog &watchdog = Watchdog::get_instance();
+        watchdog.start(RESET_TIME);              //starts watchdog timer for 30 sec. whole system resets after time is over
+        //CIEbutton.waitforpress();                //block by waiting for press
         CriticalSectionLock::disable();
-        
+        //sleep();
     }
 
 }
 //**
 void critErrbtnISR (){          //interrupt service routine for the switch at PG_1
     CIEbutton.rise(NULL);
-    t5.flags_set(2);            //trying to keep the interrupt short as possible and 
+    t5.flags_set(2);             //trying to keep the interrupt short as possible and 
                                 //let function do the work
 }
 
+//tis a test commit
 
 int main() {
     
     /*ISR button */
-        CIEbutton.rise(&critErrbtnISR);           //button A AT PG0
+        CIEbutton.rise(critErrbtnISR);           //button A AT PG0
         //set traf light 2
         redLed2.output();
         redLed2.mode(PinMode::OpenDrainNoPull);
-        redLed2 = 1;                             //as open drain, set to 1 keep off
+        redLed2 = 1;             //as open drain, set to 0 keep off
 
-
-    if (!connect()) return -1;                  // obtain network connection
-    if (!setTime()) return -1;                  // obtain time and update RTC
+    if (!connect()) return -1; // obtain network connection
+    if (!setTime()) return -1; // obtain time and update RTC
         matrix_bar start;
-        //start.BarLight(4093);
-        SDCardSetup();                          // Sets up SD card
-        btnA.rise(&Queue_Read);                 // ISR for blue button which reads SD card
-        Bluebtn.rise(&BuzzStop);                // call function to stop buzzer by passing in address
-        t1.start(GetSample);                    // sampling thread start
-        t2.start(bufferSample);                 // buffer thread start
-        t3.start(SDCardWrite);                  // sd card thread start
-        t4.start(iotazure);                     // iothub thread start
-        t5.start(CritError);                    // reset thread start
-        t6.start(matrix_display);               // matrix thread star
+        SDCardSetup();          // Sets up SD card
+        btnA.rise(&Queue_Read); //ISR for blue button which reads SD card
+        Bluebtn.rise(&BuzzStop);
+        t1.start(GetSample);    // sampling thread start
+        t2.start(bufferSample); // buffer thread start
+        t3.start(SDCardWrite);  // sd card thread start
+        t4.start(iotazure);     // iothub thread start
+        t5.start(CritError);     // reset thread 
+        t6.start(matrix_display);
         Samptick.attach(&Flag_Set, sampRate);   // ISR for control of sample rate
-        updatetmr.start();                      // timer for buffer write
+        updatetmr.start();      //timer for buffer write
         
     while (true) {
        mainQueue.dispatch_forever();            // main Queue for serial message management
